1d0b07f4c9febee783261915d97db0f4dce7e76a
Dimples_YJ <2890841438@qq.com>
2023-02-23 21:38:51 +0800

    build: 更新依赖的版本

--- unocss.config.ts
+++ unocss.config.ts
@@ -5,13 +5,13 @@ import { defineConfig } from 'unocss'
 export default defineConfig({
   presets: [
     // https://github.com/MellowCo/unocss-preset-weapp
-    presetWeapp() as any,
+    presetWeapp(),
   ],
   transformers: [
     // https://github.com/MellowCo/unocss-preset-weapp/tree/main/src/transformer/transformerAttributify
     transformerAttributify(),
 
     // https://github.com/MellowCo/unocss-preset-weapp/tree/main/src/transformer/transformerClass
-    transformerClass() as any,
+    transformerClass(),
   ],
 })
