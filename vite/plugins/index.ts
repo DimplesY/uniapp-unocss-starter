bd2515ee4f01495d7ce2864f43ef48bc94977f85
Dimples_YJ <2890841438@qq.com>
2024-11-29 16:56:26 +0800

    chore: remove comment

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -1,10 +1,8 @@
 import uni from '@dcloudio/vite-plugin-uni'
-// import createUnoCSS from './unocss'
 import createAutoImport from './auto-import'
 
 export default function createVitePlugins() {
   const vitePlugins = [uni()]
-  // vitePlugins.push(createUnoCSS())
   vitePlugins.push(createAutoImport())
   return vitePlugins
 }
